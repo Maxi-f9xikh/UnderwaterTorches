9cdc568735355018db32e9d663e3800102f8021b
Maxi <red7creeper@gmail.com>
2026-05-24 23:22:46 +0200

    Fix waterlogged default state being true for all torches
    Set default WATERLOGGED property to false via constructor injection
    so existing torches in the world don't appear waterlogged.

--- src/main/java/de/maxi/underwatertorches/mixin/TorchBlockMixin.java
+++ src/main/java/de/maxi/underwatertorches/mixin/TorchBlockMixin.java
@@ -10,12 +10,20 @@ import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Properties;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TorchBlock.class)
 public abstract class TorchBlockMixin extends Block implements Waterloggable {
 
 	private TorchBlockMixin(Settings settings) {
 		super(settings);
+	}
+
+	@Inject(method = "<init>", at = @At("TAIL"))
+	private void underwatertorches$setDefaultWaterlogged(CallbackInfo ci) {
+		this.setDefaultState(this.getDefaultState().with(Properties.WATERLOGGED, false));
 	}
 
 	@Override
